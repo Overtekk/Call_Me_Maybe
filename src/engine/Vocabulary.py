8d1fc82378461773982220716bec0eb83353d60e
Romain Andrieux <roandrie@c1r3p1.42lehavre.fr>
2026-04-13 17:24:55 +0200

    t

--- src/engine/Vocabulary.py
+++ src/engine/Vocabulary.py
@@ -6,11 +6,11 @@
 #  By: roandrie <roandrie@student.42lehavre.fr   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/04/13 11:41:33 by roandrie        #+#    #+#               #
-#  Updated: 2026/04/13 11:42:08 by roandrie        ###   ########.fr        #
+#  Updated: 2026/04/13 16:46:27 by roandrie        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, PrivateAttr
 
 
 class Vocabulary(BaseModel):
@@ -22,3 +22,5 @@ class Vocabulary(BaseModel):
         description="The state of the debug mode",
         default=False
     )
+
+    _vocab_path: str = PrivateAttr()
